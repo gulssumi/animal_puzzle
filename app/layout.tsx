29bd8634ac3ac2cbc79ec17843d03d512d2851c9
gulssumi <gulssumi@gmail.com>
2026-05-04 18:26:11 +0900

    Use plain script tag for AdSense in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { I18nProvider } from "@/lib/i18n";
-import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,11 +29,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        <Script
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3291789386215765"
           crossOrigin="anonymous"
-          strategy="beforeInteractive"
         />
       </head>
       <body className="min-h-full flex flex-col">
